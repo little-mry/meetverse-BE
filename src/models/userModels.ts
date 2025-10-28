8e6ff04425a15b3d5bbb60e84e211542ece153f0
veleii <vendelanorinder@hotmail.se>
2025-10-28 14:38:38 +0100

    usertypes and usermodels

--- src/models/userModels.ts
+++ src/models/userModels.ts
@@ -1,5 +1,6 @@
 import { Schema, model } from "mongoose";
 import type { IUser } from "../types/userTypes";
+import bcrypt from "bcryptjs";
 
 const userSchema = new Schema<IUser>(
   {
@@ -11,5 +12,30 @@ const userSchema = new Schema<IUser>(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("passwordHash")) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.passwordHash = await bcrypt.hash(this.passwordHash, salt);
+    next();
+  } catch (err) {
+    next(err as Error);
+  }
+});
+
+userSchema.methods.comparePassword = async function (password: string) {
+  return bcrypt.compare(password, this.passwordHash);
+};
+
+userSchema.set("toJSON", {
+  transform: (_doc, ret: any) => {
+    ret.id = ret._id?.toString();
+    delete ret._id;
+    delete ret.passwordHash;
+    return ret;
+  },
+});
 
 export default model<IUser>("User", userSchema);
