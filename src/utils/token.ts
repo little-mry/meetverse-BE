34cc6c7258966df249817d958ce7b9559c661b47
Lilla Mry <maria.kjellholm@gmail.com>
2025-10-29 10:15:04 +0100

    fixes in prettier congif and eslint

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,19 +1,19 @@
-import * as jose from "jose";
+import * as jose from 'jose';
 
 export const generateToken = async (userId: string): Promise<string> => {
-	const secret = process.env.JWT_SECRET;
+  const secret = process.env.JWT_SECRET;
 
-	if (!secret) {
-		throw new Error("SECRET is not defined in env variables");
-	}
+  if (!secret) {
+    throw new Error('SECRET is not defined in env variables');
+  }
 
-	const secretKey = new TextEncoder().encode(secret);
+  const secretKey = new TextEncoder().encode(secret);
 
-	const jwt = await new jose.SignJWT({ id: userId })
-		.setProtectedHeader({ alg: "HS256" })
-		.setIssuedAt()
-		.setExpirationTime("1h")
-		.sign(secretKey);
+  const jwt = await new jose.SignJWT({ id: userId })
+    .setProtectedHeader({ alg: 'HS256' })
+    .setIssuedAt()
+    .setExpirationTime('1h')
+    .sign(secretKey);
 
-	return jwt;
+  return jwt;
 };
